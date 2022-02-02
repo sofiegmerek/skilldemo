54bc8f95f88ed7b1a9708256f46daf843862f637
sofie <sofie.e.gmerek@gmail.com>
2022-02-02 14:18:19 -0800

    add failing test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,4 +1,8 @@
+
 public class SkillDemo{
 
+public static int add(int a, int b){
+    return a+b;
+}
 
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,12 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void test1(){
+        assertEquals(3, SkillDemo.add(1, 1));
+
+    }
+    
+}
